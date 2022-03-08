48090439091c399bfb19bdb4332fe61e028ef175
tolafad <adetola.fadahunsi@constant.co>
2022-03-08 03:00:31 +0000

    change to https

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const USER_API_BASE_URL = "http://buddy-env.eba-epkynjas.eu-west-1.elasticbeanstalk.com/api/v1/buddy-system";
+const USER_API_BASE_URL = "https://buddy-env-1.eu-west-1.elasticbeanstalk.com/api/v1/buddy-system";
 
 class UserService {
 
